5378ac434f3ca4505b620582e189d593a8cd5ad6
Remy Fayet <remy.fayet@telecom-paris.fr>
2025-01-26 17:29:28 +0100

    fix assembly on conveyors

--- bulle.gd
+++ bulle.gd
@@ -33,6 +33,8 @@ var pathFollower: PathFollow2D
 #func _process(delta):
 	#pass
 
+var conveyors_data = {}
+
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton  and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed: 
