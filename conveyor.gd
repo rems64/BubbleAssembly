a477b1861d08286d0d026e7875e87e11e1a00ae8
Remy Fayet <remy.fayet@telecom-paris.fr>
2025-01-26 15:19:00 +0100

    conveyor held fix

--- conveyor.gd
+++ conveyor.gd
@@ -10,7 +10,7 @@ func _process(delta: float) -> void:
 	var traction = global_transform.x.normalized();
 #
 	for bubble in bubbles:
-		if (bubble.currently_processed):
+		if (bubble.held):
 			continue
 		var correction = Vector2(0, to_local(bubble.position).y).rotated(rotation)
 		var force = (traction - 0.01*correction).normalized() * 100 * delta
