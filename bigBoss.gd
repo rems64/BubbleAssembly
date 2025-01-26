bfd9d4c9f5187fc181c6c57ae7d1f18a826b7a4d
Remy Fayet <remy.fayet@telecom-paris.fr>
2025-01-26 18:01:41 +0100

    fix music

--- bigBoss.gd
+++ bigBoss.gd
@@ -1,20 +1,12 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$accueil.show()
-	$SceneRoot.hide()
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
 func _on_start_cliqued() -> void:
-	remove_child($accueil)
-	$SceneRoot.show()
+	get_tree().change_scene_to_file("res://level01.tscn")
 
 func dijkstra(bulleDep : Node) :
 	var liste_voisins = [bulleDep]
