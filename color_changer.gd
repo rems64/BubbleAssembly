9cbf5db3e2c053bad04abbf404068fd2c8ce7dcb
Remy Fayet <remy.fayet@telecom-paris.fr>
2025-01-31 17:53:39 +0100

    change how color_changer is handled

--- color_changer.gd
+++ color_changer.gd
@@ -6,7 +6,7 @@ extends Node2D
 
 @onready var area_in: Area2D = $"Area In"
 @onready var area_out: Area2D = $"Area Out"
-@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var progress_bars: VBoxContainer = $VBoxContainer
 
 var processing_bubbles = []
 
@@ -14,6 +14,7 @@ var processing_bubbles = []
 func _ready() -> void:
 	pass # Replace with function body.
 
+signal spawn_bubble
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,8 +24,13 @@ func _process(delta: float) -> void:
 			if bubble[0] == null:
 				processing_bubbles.erase(bubble)
 				continue
-			bubble[0].reparent(get_tree().get_current_scene())
-			var bub = bubble[0].get_child(0);
+			var scene = preload("res://bubulle.tscn")
+			var scene_instance = scene.instantiate()
+			scene_instance.set_name("scene")
+			get_tree().get_current_scene().add_child(scene_instance)
+			var bub = scene_instance.get_child(0);
+			scene_instance.global_position = area_out.global_position
+			bub.modulate = bubble[0]
 			if (bub.modulate == Color.DIM_GRAY):
 				pass
 			elif (bub.modulate == Color.WHITE):
@@ -33,30 +39,31 @@ func _process(delta: float) -> void:
 				bub.modulate = Color(min(bub.modulate.r+color.r, 1), min(bub.modulate.g+color.g, 1), min(bub.modulate.b+color.b, 1))
 				if (bub.modulate == Color.WHITE):
 					bub.modulate = Color.DIM_GRAY #Color.BLACK
-			bubble[0].show();
-			bubble[0].currently_processed = false
-			bubble[0].collision_layer = bubble[3]
 			processing_bubbles.erase(bubble)
-			progress_bar.value = 0.
+			bubble[3].value = 0.
+			bubble[3].queue_free()
+			spawn_bubble.emit()
+			
 		else:
 			bubble[1] -= delta
-			progress_bar.value = 100.*(1-bubble[1]/bubble[2])
+			bubble[3].value = 100.*(1-bubble[1]/bubble[2])
 
 func _on_area_in_body_entered(body: Node2D) -> void:
 	if (body is RigidBody2D):
 		if body.held || body.in_held_block || body.currently_processed:
 			return
-		for bubble in processing_bubbles:
-			if bubble[0]==body:
-				return
 		
 		var duration = randf_range(process_duration-process_duration_random, process_duration+process_duration_random)
 		#body.reparent(self)
-		
-		processing_bubbles.push_back([body, duration, duration, body.collision_layer])
+		var pb = ProgressBar.new()
+		pb.show_percentage = false
+		pb.modulate = color
+		progress_bars.add_child(pb)
+		processing_bubbles.push_back([body.get_child(0).modulate, duration, duration, pb])
 		var dp = 1.5*(area_out.global_position - area_in.global_position)
-		body.position += dp
-		#body.freeze = true;
-		body.hide()
-		body.collision_layer = 0
-		body.currently_processed = true
+		body.free()
+		#body.position += dp
+		##body.freeze = true;
+		#body.hide()
+		#body.collision_layer = 0
+		#body.currently_processed = true
